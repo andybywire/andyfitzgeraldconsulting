14c3c20cf48452feb10fba11cf53510dd6d4e1fb
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2022-09-15 06:47:53 -0700

    Block out review page data

--- web/_data/clients.js
+++ web/_data/clients.js
@@ -8,17 +8,3 @@ module.exports =  async function() {
     }
   `)
 }
-
-// async function getClients () {
-//   const filter = groq`*[_type == "skosConcept"]`
-//   const projection = groq`{
-//     prefLabel,
-//     "scheme": scheme->title
-//   }`
-//   const order = `| order(prefLabel)`
-//   const query = [filter, projection, order].join(' ')
-//   const data = await client.fetch(query).catch(err => console.error(err))
-//   return data
-// }
-
-// module.exports = getClients
---
+++ web/_data/reviews.js
@@ -0,0 +1,25 @@
+const groq = require('groq')
+const client = require('../utils/sanityClient')
+const BlocksToMarkdown = require('@sanity/block-content-to-markdown')
+const serializers = require('../utils/serializers')
+const overlayDrafts = require('../utils/overlayDrafts')
+const hasToken = !!client.config().token
+
+function generateReview (review) {
+  return {
+    ...review,
+    body: BlocksToMarkdown(review.body, { serializers, ...client.config() })
+  }
+}
+
+async function getReview () {
+  const filter = groq`*[_type == "review" && !(_id in path("drafts.**"))]`
+  const projection = groq`{...}`
+  const query = [filter, projection].join(' ')
+  const docs = await client.fetch(query).catch(err => console.error(err))
+  const reducedDocs = overlayDrafts(hasToken, docs)
+  const prepareReviews = reducedDocs.map(generateReview)
+  return prepareReviews
+}
+
+module.exports = getReview
