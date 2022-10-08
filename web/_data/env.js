bef4d4710701a6d41a7d9f55ede609f4ecd50949
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2022-10-08 10:21:17 -0700

    Add recaptcha site key to template

--- web/_data/env.js
+++ web/_data/env.js
@@ -1,7 +1,6 @@
 module.exports = function() {
   return {
     environment: process.env.ELEVENTY_ENVIRONMENT || "development",
-    recaptchaKey: process.env.RECAPTCHA_SITEKEY,
-    recaptchaSecret: process.env.RECAPTCHA_SECRET,
+    recaptchaSecret: process.env.RECAPTCHA_SECRET
   };
 };
