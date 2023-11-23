04397530868c91d9c993cf7be9fa882aaecf1778
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2023-11-23 08:56:00 -0800

    Migrate and order desk structure

--- studioV3/sanity.config.ts
+++ studioV3/sanity.config.ts
@@ -4,6 +4,10 @@ import {taxonomyManager} from 'sanity-plugin-taxonomy-manager'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {codeInput} from '@sanity/code-input'
+import {RiSettings4Line} from 'react-icons/ri'
+
+const hiddenDocTypes = (listItem: any) =>
+  !['media.tag', 'settings', 'skosConcept', 'skosConceptScheme'].includes(listItem.getId())
 
 export default defineConfig({
   name: 'default',
@@ -13,7 +17,22 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [
-    deskTool(),
+    deskTool({
+      structure: (S) => {
+        return S.list()
+          .title('Content')
+          .items([
+            ...S.documentTypeListItems().filter(hiddenDocTypes),
+            S.divider(),
+            S.listItem()
+              .title('Settings')
+              .icon(RiSettings4Line)
+              .child(S.document().schemaType('settings').documentId('settings')),
+            S.documentTypeListItem('skosConceptScheme').title('Taxonomy Schemes'),
+            S.documentTypeListItem('skosConcept').title('Concepts'),
+          ])
+      },
+    }),
     taxonomyManager({
       baseUri: 'https://andyfitzgeraldconsulting.com/',
     }),
--- studioV3/schemas/index.ts
+++ studioV3/schemas/index.ts
@@ -1,28 +1,28 @@
 import article from './documents/article'
 import caseStudy from './documents/caseStudy'
-import review from './documents/review'
 import client from './documents/client'
 import collection from './documents/collection'
 import event from './documents/event'
+import review from './documents/review'
 import service from './documents/service'
 import settings from './documents/settings'
 import singleton from './documents/singleton'
 
 import banner from './objects/banner'
-import figure from './objects/figure'
 import cta from './objects/cta'
+import figure from './objects/figure'
 
 export const schemaTypes = [
   article,
   caseStudy,
-  review,
-  client,
-  collection,
-  event,
   service,
-  settings,
+  client,
+  review,
+  event,
+  collection,
   singleton,
+  settings,
   banner,
-  figure,
   cta,
+  figure,
 ]
