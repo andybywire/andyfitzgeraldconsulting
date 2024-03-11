56e65f60f6cfdace7b4866a4a26d18a0f496ca1b
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2024-03-11 10:03:46 -0700

    Filter client review list

--- web/_data/clients.js
+++ web/_data/clients.js
@@ -3,8 +3,9 @@ import groq from 'groq';
 
 export default async function() {
   return await client.fetch(groq`
-    *[_type == "client"] | order(name desc){
-      ...
+    *[_type == "client"] | order(name){
+      ...,
+      "reviews": count(*[_type == 'review' && references(^._id)])
     }
   `)
 }
