6a217a57d77207a1b2bcc8d0d6c26e5e3f303032
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2024-04-04 15:13:10 -0700

    Add image styles & increment serviceworker

--- web/serviceworker.js
+++ web/serviceworker.js
@@ -18,7 +18,7 @@ addEventListener('fetch', (fetchEvent) => {
 	); // end respondWith
 }); // end addEventListener
 
-const version = 'v0.81';
+const version = 'v0.82';
 const staticCacheAFC = version + 'staticfiles';
 
 addEventListener('install', (installEvent) => {
