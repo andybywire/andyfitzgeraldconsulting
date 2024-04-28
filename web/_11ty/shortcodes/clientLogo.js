793aacc8ab4d25c90913e15af1adc26a7d03e734
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2024-04-28 10:59:17 -0700

    Add responsive image shortcode for index page client logos

--- web/_11ty/shortcodes/clientLogo.js
+++ web/_11ty/shortcodes/clientLogo.js
@@ -2,7 +2,13 @@ import urlFor from '../../utils/imageUrl.js';
 
 const sizeArray = ['375', '425', '768'];
 const aspect = 0.5625; // 16:9 aspect ratio
-
+/**
+ * Client Logo shortcode
+ * Used on Home and Case Study pages
+ * @todo: Add support for custom sizes, or break into two shortcodes. The index images are 2 col wide (d/t), where case study are 3 col. 
+ * @param {object} image - Image object from Sanity
+ * @returns {string} - HTML string for the image
+ */
 export default function (image) {
 	const firstSize = sizeArray[0];
 	const lastSize = sizeArray[sizeArray.length - 1];
@@ -27,3 +33,5 @@ export default function (image) {
         alt="${altText}"
       >`;
 }
+
+// 40rem breakpoint: max-width (1024px), minus 1.5vw * 13 (1.5vw for each of the 12 columns, plus 1.5vw for the gutter)
