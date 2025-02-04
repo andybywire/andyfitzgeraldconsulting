ef920e443a80facb341d2d66e235e0f09374ed3a
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-02-04 15:34:32 -0800

    working taxo-tool scripts

---
+++ studio/migrations/import-taxonomy-terms/generated-index.ts
@@ -0,0 +1,22 @@
+import {defineMigration, patch, at, setIfMissing} from 'sanity/migrate'
+
+/**
+ * this migration will set `Default title` on all documents that are missing a title
+ * and make `true` the default value for the `enabled` field
+ */
+export default defineMigration({
+  title: 'import taxonomy terms',
+  documentTypes: ["skosConceptScheme", "skosConcept"],
+
+  async *migrate(documents, context) {
+    for await (const document of documents()) {
+      yield patch(document._id, [
+        at('title', setIfMissing('Default title')),
+        at('enabled', setIfMissing(true)),
+      ])
+    }
+  }
+})
+
+
+
