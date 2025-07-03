4f2d969de6e3dde51f88f90b0396805aa095c803
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-07-03 14:17:07 -0700

    Increment serviceworker

--- web/serviceworker.js
+++ web/serviceworker.js
@@ -18,7 +18,7 @@ addEventListener('fetch', (fetchEvent) => {
 	); // end respondWith
 }); // end addEventListener
 
-const version = 'v0.91';
+const version = 'v0.92';
 const staticCacheAFC = version + 'staticfiles';
 
 addEventListener('install', (installEvent) => {
