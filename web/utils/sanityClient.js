956328bbaaa4fc6c77804e35c0ac82d7f4160934
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-08-02 12:50:48 -0700

    Update client to use "published" perspective

--- web/utils/sanityClient.js
+++ web/utils/sanityClient.js
@@ -4,5 +4,6 @@ export const client = createClient({
 	projectId: '7v0qvet6',
 	dataset: 'production',
 	useCdn: false, // bypasses edge cache
-	apiVersion: '2024-01-01', // current date targets the latest version
+	apiVersion: '2025-08-02', // current date targets the latest version
+	perspective: 'published',
 });
