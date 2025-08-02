38e6ad27a74652b85bed6e6f380fe04255f6c8d6
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-08-02 14:13:12 -0700

    Remove unused objects and data sources

--- studio/schemas/objects/cta.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    name: 'cta',
-    title: 'Call to Action',
-    type: 'text',
-    rows: 2,
-    description: 'Set a custom call to action for this page type. If empty, default values apply. 60 - 100 characters'
-  }
