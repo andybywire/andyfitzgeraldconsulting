d104863e394f5097db0770e4158b4e23fc12a6ad
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-08-01 19:14:18 -0700

    update schemaTypes

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,10 +1,8 @@
 import article from './documents/article'
 import caseStudy from './documents/caseStudy'
 import client from './documents/client'
-import collection from './documents/collection'
 import event from './documents/event'
 import review from './documents/review'
-import service from './documents/service'
 import settings from './documents/settings'
 import singleton from './documents/singleton'
 
@@ -15,11 +13,9 @@ import figure from './objects/figure'
 export const schemaTypes = [
   article,
   caseStudy,
-  service,
   client,
   review,
   event,
-  collection,
   singleton,
   settings,
   banner,
