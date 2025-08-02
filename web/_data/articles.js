ba3daad5a78e5bd9c30ea59108df48d7b7786661
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-08-02 12:34:35 -0700

    Rank related articles by number of shared tags

--- web/_data/articles.js
+++ web/_data/articles.js
@@ -4,20 +4,20 @@ import { toHTML } from '@portabletext/to-html';
 import { afcComponents } from '../utils/serializers.js';
 
 function generateArticle(article) {
-  return {
+	return {
 		...article,
 		body: toHTML(article.bodyText, { components: afcComponents }),
 		lede: toHTML(article.lede, { components: afcComponents }),
-    atGlance: toHTML(article.atGlance, { components: afcComponents }),
-    whatDid: toHTML(article.whatDid, { components: afcComponents }),
-    condensedReviewBody: toHTML(article?.review?.condensedBody, { components: afcComponents }),
-    projectGoal: toHTML(article.projectGoal, { components: afcComponents }),
-    projectOutcome: toHTML(article.projectOutcome, { components: afcComponents }),
-    projectApproach: toHTML(article.projectApproach, { components: afcComponents })
+		atGlance: toHTML(article.atGlance, { components: afcComponents }),
+		whatDid: toHTML(article.whatDid, { components: afcComponents }),
+		condensedReviewBody: toHTML(article?.review?.condensedBody, { components: afcComponents }),
+		projectGoal: toHTML(article.projectGoal, { components: afcComponents }),
+		projectOutcome: toHTML(article.projectOutcome, { components: afcComponents }),
+		projectApproach: toHTML(article.projectApproach, { components: afcComponents }),
 	};
 }
 
-// Add to Case Study: 
+// Add to Case Study:
 
 async function getArticles() {
 	// Learn more: https://www.sanity.io/docs/data-store/how-queries-work
@@ -60,19 +60,26 @@ async function getArticles() {
     "topicTags":topic[]->prefLabel,
     "categoryTag":category->prefLabel,
     canonical,
-    "relatedResources": *[(_type=='study' && references(^.category._ref) || _type=='article' && references(^.category._ref) || _type=='interview' && references(^.category._ref)) && _id != ^._id] {
-      title,
-      heroImage,
-      shortDescription,
-      "insightType": insightType->prefLabel,
-      slug,
-      pubDate,
-      _type,
-      _type == 'service' => {"tag": "Service", "path":"services"},
-      _type == 'study' => {"tag": "Case Study", "path":"case-studies"},
-      _type == 'article' => {"tag": "Article", "path":"writing"}
-    } | order(_type desc, pubDate desc) [0..3]
-  }`;
+    "relatedResources": *[
+      (_type=='caseStudy' || _type =='article')
+      && array::intersects(topic[]._ref, ^.topic[]._ref)
+      && _id != ^._id ] 
+      {
+        title,
+        "sharedTags": length(topic[] + ^.topic[]) - count(array::unique(topic[]._ref + ^.topic[]._ref)),
+        heroImage,
+        shortDescription,
+        "insightType": insightType->prefLabel,
+        "category": category->prefLabel,
+        "topics": topic[]->prefLabel,
+        slug,
+        pubDate,
+        _type,
+        _type == 'service' => {"tag": "Service", "path":"services"},
+        _type == 'study' => {"tag": "Case Study", "path":"case-studies"},
+        _type == 'article' => {"tag": "Article", "path":"writing"}
+      } | order(sharedTags desc, pubDate desc) [0..3]
+    }`;
 	const order = `| order(pubDate desc)`;
 	const query = [filter, projection, order].join(' ');
 	const docs = await client.fetch(query).catch((err) => console.error(err));
