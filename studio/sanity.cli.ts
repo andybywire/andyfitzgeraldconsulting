f574ee6023277b76534ff45f55e9435c176aa7df
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-11-01 11:08:57 -0700

    Enable studio auto updates

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,5 +5,11 @@ export default defineCliConfig({
     projectId: '7v0qvet6',
     dataset: 'production'
   },
-  studioHost: 'af-consulting'
+  studioHost: 'af-consulting',
+  deployment: {
+    autoUpdates: true,
+    appId: 'nzdgq8jqulaft9768ud9xxk9'
+  }
 })
+
+
